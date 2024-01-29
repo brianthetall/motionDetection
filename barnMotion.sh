dbcd22f01df58da0a02e6484d298c6773bb4d862
Brian Wagner <brian_wagner@milacron.com>
2024-01-28 21:52:01 -0500

    Added motion scripts for running services.

---
+++ barnMotion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.9/site-packages/
+. bin/activate
+
+while [[ 1 ]]
+do
+    /usr/home/user/motionDetection/bin/python /home/user/motionDetection/motionDetect.py rtsp://admin:admin@172.172.172.33/11
+done
